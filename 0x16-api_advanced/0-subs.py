03d1b89e95fdf54aa9ebb7e757a3b3c1d666a521
Solomon Bassey <Solomonbassey20@gmail.com>
2024-03-06 03:15:47 +0100

    Fixed stuff

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,21 +3,16 @@
     this module contains the function number_of_subscribers
 '''
 import requests
-from sys import argv
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+    """method doc"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
         return 0
-
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
