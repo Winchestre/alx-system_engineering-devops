c19dff2e8c66cf2634f65a73fb117f4ec9613cfe
Solomon Bassey <Solomonbassey20@gmail.com>
2024-03-07 14:30:40 +0100

    0-subs.py fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,9 @@ headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 def number_of_subscribers(subreddit):
     """method doc"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    response = requests.get(url, headers=headers).json()
+    subscriptions = response.get("data", {}).get("subscribers", 0)
+    return subscriptions
