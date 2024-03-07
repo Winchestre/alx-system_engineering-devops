a98297a161df71c4c15ddccffce90457d92dbad4
Solomon Bassey <Solomonbassey20@gmail.com>
2024-03-07 14:10:11 +0100

    Puppet-lint fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # Replace .phpp with .php in wp-settings.php
 exec { 'fix wordpress':
-  command	=> "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-	}
+  command  => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  }
